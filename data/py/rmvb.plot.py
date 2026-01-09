cad269cdc7591665dc7d841a4a97f6c831ea255a
MarkShevelev <mposimba@gmail.com>
2026-01-09 16:07:42 +0300

    py: rmvb.plot.py can plot up to four different lines at four panels: rho, mnt, v and bz

--- data/py/rmvb.plot.py
+++ data/py/rmvb.plot.py
@@ -1,37 +1,61 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
+import os
+
 if len(sys.argv) < 2:
-    print("Usage: python script.py <filename>")
+    print("""
+          Script to plot a comparasing graphs:
+            -- the first file is plot in black;
+            -- the second is plot in red;
+            -- the third is plot in blue;
+            -- the last is green.
+
+          Usage: python3 rmvb.cmp.plot <file1> [<file2> <file3> <file4>]
+          """)
     sys.exit(1)
 
-filename = sys.argv[1]
-h, rho, mnt, bz = np.loadtxt(filename, unpack=True, skiprows=1)
-v = mnt / rho
+files = sys.argv[1:5]
 
-fig, axs = plt.subplots(2, 2, figsize=(8, 6))
-fig.suptitle('Four Panes Example') # Add a overall title
+fig, axs = plt.subplots(2, 2, figsize=(10, 8), layout='constrained', sharex=True)
+fig.suptitle('rho mnt v bz')
 
-# Top-left pane (row 0, column 0)
-axs[0, 0].plot(h, rho, 'tab:blue')
+plot_colors = ['k','r', 'b', 'g']
+
 axs[0, 0].set_title('rho')
-axs[0, 0].set_ylim(0, 2)
+axs[0, 0].set_ylim(0.75, 2)
 
-# Top-right pane (row 0, column 1)
-axs[0, 1].plot(h, mnt, 'tab:blue')
 axs[0, 1].set_title('mnt')
 axs[0, 1].set_ylim(-1, 1)
 
-# Bottom-left pane (row 1, column 0)
-axs[1, 0].plot(h, v, 'tab:blue')
-axs[1, 0].set_title('v')
+axs[1, 0].set_title('v (mnt/rho)')
 axs[1, 0].set_ylim(-1, 1)
 
-# Bottom-right pane (row 1, column 1)
-axs[1, 1].plot(h, bz, 'tab:blue')
 axs[1, 1].set_title('bz')
 
-# 3. Adjust layout to prevent labels from overlapping
-plt.tight_layout()
+for i, filename in enumerate(files):
+    parts = os.path.basename(filename).split(".")
+    label = parts[-2] if len(parts) > 2 else filename
+
+    # Load data for the current file
+    h, rho, mnt, bz = np.loadtxt(filename, unpack=True, skiprows=1)
+    v = mnt / rho
+    
+    color = plot_colors[i]
+    
+    # Plot current file's data into the corresponding pane
+    # rho
+    axs[0, 0].plot(h, rho, color=color, label=label)
+    
+    # mnt
+    axs[0, 1].plot(h, mnt, color=color)
+    
+    # v (mnt/rho)
+    axs[1, 0].plot(h, v, color=color)
+    
+    # bz
+    axs[1, 1].plot(h, bz, color=color)
+
+axs[0, 0].legend(fontsize='small', frameon=False)
 
 plt.show()
