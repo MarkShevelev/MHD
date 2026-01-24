ebb504cd2256e7dd07f27ce19315b881f0ac03d1
MarkShevelev <mposimba@gmail.com>
2026-01-24 22:22:27 +0300

    mesh: meshes refactored to mutable Uf32 and immutable Uf32View and functions args changed to moved

--- src/mesh_st.rs
+++ src/mesh_st.rs
@@ -5,6 +5,14 @@ pub struct Uf32<'a>
   pub bz : &'a mut [f32],
 }
 
+#[derive(Clone, Copy)]
+pub struct Uf32View<'a>
+{
+  pub rho: &'a [f32],
+  pub mnt: &'a [f32],
+  pub bz : &'a [f32],
+}
+
 impl<'a> Uf32<'a>
 {
   pub fn new (
@@ -12,4 +20,46 @@ impl<'a> Uf32<'a>
   {
     Self { rho, mnt, bz }
   }
+
+  pub fn new_with_slice(
+    rho: &'a mut [f32], mnt: &'a mut [f32], bz: &'a mut [f32],
+    begin: usize, end: usize) -> Self
+  {
+    Self {
+      rho: &mut rho[begin..end],
+      mnt: &mut mnt[begin..end],
+      bz:  &mut bz [begin..end]
+    }
+  }
 }
+
+impl<'a> Uf32View<'a>
+{
+  pub fn new (
+    rho: &'a [f32], mnt: &'a [f32], bz: &'a [f32]) -> Self
+  {
+    Self { rho, mnt, bz }
+  }
+
+  pub fn new_with_slice(
+    rho: &'a [f32], mnt: &'a [f32], bz: &'a [f32],
+    begin: usize, end: usize) -> Self
+  {
+    Self {
+      rho: &rho[begin..end],
+      mnt: &mnt[begin..end],
+      bz:  &bz [begin..end]
+    }
+  }
+}
+
+impl<'a, 'b> From<&'a Uf32<'b>> for Uf32View<'a> {
+    fn from(src: &'a Uf32<'b>) -> Self {
+        Uf32View {
+            rho: src.rho,
+            mnt: src.mnt,
+            bz:  src.bz,
+        }
+    }
+}
+
